41c2217abe7a2f6000c1776b36e8f39478bda46f
Pablo Baltodano Madrigal <Pablo Baltodano Madrigal>
2016-11-10 04:18:40 -0600

    First test query

--- models/song.go
+++ models/song.go
@@ -1,6 +1,52 @@
 package models
 
 import (
-	"fmt"
+	"log"
 	"database/sql"
 )
+
+const (
+	// Name of the table for the song model
+	SongTableName = "songs"
+	// Names of every column of the model
+	
+	SongArtistCol = "artist"
+	SongNameCol = "song"
+	SongGenreCol = "genre"
+	SongLengthCol = "length"
+)
+
+// Person is the database model for a person
+type Songs struct {
+	Artist string
+	Name  string
+	Genre  string
+	Length  uint
+}
+
+func Query(db *sql.DB, artist string, song string, genre uint, length uint, result *Songs) error {
+	row := db.QueryRow(
+		fmt.Sprintf(
+				"SELECT * FROM %s WHERE %s=? AND %s=? AND %s=?",
+			SongTableName,
+			SongArtistCol,
+			SongNameCol,
+			SongGenreCol,
+			SongLengthCol,
+		),
+		artist,
+		song,
+		genre,
+		length,
+	)
+	var resultSongArtist, resultSongName, resultSongGenre string
+	var resultSongLength uint
+	if err := row.Scan(&resultSongArtist, &resultSongName, &resultSongGenre, resultSongLength); err != nil {
+		return err
+	}
+	result.Artist = resultSongArtist
+	result.Name = resultSongName
+	result.Genre = resultSongGenre
+	result.Length = resultSongLength
+	return nil
+}
--- project.go
+++ project.go
@@ -3,16 +3,31 @@ package main
 import (
 	"log"
 	"database/sql"
+
+	"github.com/pbaltodanom/DataDeck/models"
 	_ "github.com/mattn/go-sqlite3"
 )
 
+func InitDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+	if err != nil { panic(err) }
+	if db == nil { panic("db nil") }
+	return db
+}
+
+
 func main() {
-	db, err := sql.Open("sqlite3",
-		"https://s3.amazonaws.com/bv-challenge/jrdd.db")
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Printf("se realizón conexión")
-	}
+	const dbpath = "https://s3.amazonaws.com/bv-challenge/jrdd.db"
+
+	db := InitDB(dbpath)
 	defer db.Close()
+
+	me := models.Songs{Artist: "Santana", Name:  "Smooth", Genre: "Pop", Length: "167"}
+
+	log.Printf("SELECT")
+	selected := models.Songs{}
+	if err := models.Query(db, me.Artist, me.Name, me.Genre, me.Length); err != nil {
+		log.Fatalf("Error selecting person from the DB (%s)", err)
+	}
+	log.Printf("Selected %+v from the DB", selected)
 }
