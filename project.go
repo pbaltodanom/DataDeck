4abe3724e426f60b1591fbc7530d2b37b663fd04
Pablo Baltodano Madrigal <Pablo Baltodano Madrigal>
2016-11-14 09:50:05 -0600

    last commit - just some details

--- project.go
+++ project.go
@@ -4,16 +4,17 @@ package main
 
 //---------------- IMPORTS ---------------- 
 import (
-	"DataDeck/api"
-	//"github.com/pbaltodanom/DataDeck/api"
-	"log"	
+	"github.com/pbaltodanom/DataDeck/api"
+	"fmt"	
 	"net/http"		
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	//ListenAndServe starts an HTTP server with a given address and handler.	
-	log.Fatal(http.ListenAndServe(":12345", api.Handlers()))
+	fmt.Println("Server starting")
+	//ListenAndServe starts an HTTP server with a given address and handler.
+	//log.Fatal(http.ListenAndServe(":12345", api.Handlers()))
+	http.ListenAndServe(":3000", api.Handlers())
 }
 
